3d2398c724aa10210917684dbbe9c2cffbadde52
muschellij2 <muschellij2@gmail.com>
2018-08-24 20:41:35 -0400

    Adding in the flexconn peaking.

--- R/normalize_image.R
+++ R/normalize_image.R
@@ -87,3 +87,71 @@ image_peak <- function(vol,
   return(L)
 }
 
+
+#' @rdname normalize_image
+#' @export
+flexconn_image_peak <- function(vol,
+                                contrast = c("T1", "PD", "T2", "FLAIR", "FL"),
+                                verbose = TRUE,
+                                gridsize = 80L) ## scipy rounds up from 80 to 128)
+{
+  contrast = toupper(contrast)
+  contrast = match.arg(contrast)
+  contrast = substr(contrast, 1, 2)
+
+  sm = reticulate::import("statsmodels.api")
+  np = reticulate::import("numpy")
+  ss = reticulate::import("scipy.signal")
+
+  vol = check_nifti(vol, allow.array = TRUE)
+
+  temp <- vol[vol != 0]
+
+  temp = sort(temp)
+  index = length(temp) * 0.99
+  frac = index %% 1
+  i = floor(index)
+  j = ceiling(index)
+  q = temp[i] + (temp[j] - temp[i]) * frac
+
+  temp <- temp[temp <= q]
+  bw <- q / 80
+  if (verbose) {
+    message(paste0("99th quantile is ",
+                   round(q, 3),
+                   ", bandwidth =",
+                   round(bw, 3),
+                   ", gridsize =",
+                   round(gridsize, 3)
+    ))
+  }
+
+  kde = sm$nonparametric$KDEUnivariate(temp)
+
+  kde$fit(kernel='gau', bw=bw, gridsize=gridsize, fft=TRUE)
+  x_mat = 100.0 * kde$density
+  y_mat = kde$support
+
+  indx = ss$argrelextrema(x_mat, np$greater)
+  indx = np$asarray(indx, dtype=np$int)
+  heights = x_mat[indx]
+  peaks = y_mat[indx]
+
+  if (verbose) {
+    message(paste0(length(peaks), " peaks found.", "\n"))
+  }
+  if (contrast == "T1") {
+    peak <- peaks[length(peaks)]
+  } else if (contrast %in% c("T2", "PD", "FL")) {
+    # peak_height <- which.max(heights)
+    peak <- peaks[which.max(heights)]
+  }
+  if (verbose) {
+    message(paste("Peak found at", peak, "for", contrast))
+  }
+  L = list(peak = peak,
+           q99 = q,
+           bandwidth = bw)
+  return(L)
+
+}
